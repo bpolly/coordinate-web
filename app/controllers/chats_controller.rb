a4c58f6a25f50ba8d855e1304a9ebfbac5cb71d5
Braden Polly <bpolly6@gmail.com>
2017-03-28 13:43:40 -0400

    Start messages and chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,23 @@
 class ChatsController < ApplicationController
+  before_action :lookup_user
+
+  def create
+    @chat = Chat.new
+    @chat.user = @user
+    @chat.distance_limit = params[:distance_limit] || 10 #default
+    @chat.latitude = params[:latitude]
+    @chat.longitude = params[:longitude]
+
+    if @chat.save
+      render :json => @chat
+    else
+      head 500
+    end
+  end
+
+  private
+
+  def lookup_user
+    @user = User.find_by(mac_address: params[:mac_address])
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+  before_action :lookup_user
+  before_action :lookup_chat
+
+  private
+
+  def lookup_user
+    @user = User.find_by(mac_address: params[:mac_address])
+  end
+
+  def lookup_chat
+    if(params[:chat_id])
+      @chat = Chat.find(params[:chat_id])
+    else
+      # do something
+    end
 end
