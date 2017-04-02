50f60bb6f14dcc1af2a5f9d3a77a39e47c0191ff
Ben Bloom <bbloom22@gmail.com>
2017-04-02 19:00:10 -0400

    Add update to location of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :lookup_user
+
+  def update_location
+    @user.latitude = params[:latitude]
+    @user.longitude = params[:longitude]
+
+    if @user.save
+      render :json => @user
+    else
+      head 500
+    end
+  end
+
+  private
+
+  def lookup_user
+    @user = User.find_or_create_by(mac_address: params[:mac_address])
+  end
+
 end
