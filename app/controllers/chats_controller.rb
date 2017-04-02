7898c854cbf9aa121587ad989453fa3df4a21057
Braden Polly <bpolly6@gmail.com>
2017-04-02 18:03:30 -0400

    Skip CSRF authenticity check

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,5 @@
 class ChatsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :lookup_user
 
   def create
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :lookup_user
   before_action :lookup_chat
 
