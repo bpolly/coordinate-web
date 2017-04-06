eb198870582484458132810099d2bd7a4ab40ad5
Braden Polly <bpolly6@gmail.com>
2017-04-06 13:22:19 -0400

    Add username generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,26 @@ class User < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude
   has_many :chats
   has_many :messages
+
+  before_create :generate_username
+
+  private
+  def generate_username
+    num = rand(4)
+
+    case num
+    when 0
+      self.username = Faker::HeyArnold.character
+    when 1
+      self.username = Faker::GameOfThrones.character
+    when 2
+      self.username = Faker::Superhero.name
+    when 3
+      self.username = Faker::HarryPotter.character
+    when 4
+      self.username = Faker::Ancient.god
+    when 5
+      self.username = Faker::Friends.character
+    end
+  end
 end
