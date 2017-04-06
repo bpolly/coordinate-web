7b6f15ae576d8c1e4fbc5cb9aad8a4ba6e10ed83
Braden Polly <bpolly6@gmail.com>
2017-04-06 13:01:10 -0400

    Add username to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405164104) do
+ActiveRecord::Schema.define(version: 20170406170045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170405164104) do
     t.datetime "updated_at",  null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "username"
   end
 
   add_foreign_key "chats", "users"
