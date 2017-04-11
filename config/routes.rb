c60f04a15c4eb28b50b82a0c343a17bd48322bfc
Ben Bloom <bbloom22@gmail.com>
2017-04-10 21:04:19 -0400

    Add default json for updatelocation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/get_chats', to: 'chats#get_chats', :defaults => { :format => 'json' }
-  post '/users/update_location', to: 'users#update_location'
+  post '/users/update_location', to: 'users#update_location', :defaults => { :format => 'json' }
 
   resources :chats, :defaults => { :format => 'json' }
   resources :messages, :defaults => { :format => 'json' }
